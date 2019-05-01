ea14f3a4dd5e72e6b14f4028be20ab5359a578f2
Alek2750 <alek2750@gmail.com>
2019-05-01 21:09:06 +0200

    basic get & delete facade

--- src/main/java/facade/CarFacade.java
+++ src/main/java/facade/CarFacade.java
@@ -1,6 +1,7 @@
 package facade;
 
 import entity.Car;
+import entity.Location;
 import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
@@ -11,10 +12,10 @@ import javax.persistence.EntityManagerFactory;
  * @author ale12
  */
 public class CarFacade {
-    
+
     private static CarFacade instance;
     private static EntityManagerFactory emf;
-    
+
     public List<Car> getAllCars() {
         EntityManager em = emf.createEntityManager();
         List<Car> c = new ArrayList();
@@ -27,5 +28,56 @@ public class CarFacade {
             em.close();
         }
     }
-    
+//    public class CustomerFacade {
+//  EntityManagerFactory emf;
+//
+//  public CustomerFacade(EntityManagerFactory emf) {
+//    this.emf = emf;
+//  }
+//
+//  EntityManager getEntityManager(){ 
+//     return emf.createEntityManager();
+//  }
+//
+//  // Use this template for a method that uses the
+//  // EntityManager
+//  public Customer getCustomer(int id){
+//    EntityManager em = getEntityManager();
+//    try{
+//      // Use the entity manager  
+//    }
+//    finally{
+//      em.close();
+//    }
+//  }
+
+    public Car getCarByID(int id) {
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            em.getTransaction().begin();
+            Car c = em.find(Car.class, id);
+
+            em.getTransaction().commit();
+            return c;
+
+        } finally {
+            em.close();
+        }
+    }
+
+    public Car deleteCarByID(int id) {
+        EntityManager em = emf.createEntityManager();
+        
+        try {
+            em.getTransaction().begin();
+            Car c = em.find(Car.class, id);
+            em.remove(c);
+            em.getTransaction().commit();
+            return c;
+        } finally {
+            em.close();
+        }
+
+    }
 }
