3a836527d1181f2d595c79ce52949fe745572481
Johachim <joachimgul@yahoo.dk>
2019-05-01 13:24:03 +0200

    start på carfacade

--- src/main/java/facade/CarFacade.java
+++ src/main/java/facade/CarFacade.java
@@ -1,14 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package facade;
+
+import entity.Car;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 
 /**
  *
  * @author ale12
  */
 public class CarFacade {
+    
+    private static CarFacade instance;
+    private static EntityManagerFactory emf;
+    
+    public List<Car> getAllCars() {
+        EntityManager em = emf.createEntityManager();
+        List<Car> c = new ArrayList();
+        try {
+            em.getTransaction().begin();
+            c = em.createNamedQuery("cars.findAll").getResultList();
+            em.getTransaction().commit();
+            return c;
+        } finally {
+            em.close();
+        }
+    }
     
 }
