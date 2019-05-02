2e5e6499bdda93fa59cfb4b2f21d1fba0fa8fb6e
Alek2750 <alek2750@gmail.com>
2019-05-02 11:08:34 +0200

    Revert "moved travis to backend folder"
    This reverts commit 3aac5a860db386d54206edaa3fb93247e97b66eb.

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,68 +1,28 @@
 import React, { Component } from 'react';
-import { Redirect, Link, NavLink, Route, Switch } from "react-router-dom";
-import Login, {fakeAuth} from "./Login";
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        
-          <ul className="header">
-            <li>
-              <NavLink exact to="/">Home</NavLink>
-            </li>
-            <li>
-              <NavLink to="/login">Login</NavLink>
-            </li>
-            <li>
-              <NavLink to="/admin">Admin area</NavLink>
-            </li>
-          </ul>
-        
-
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/login" component={Login} />
-          <PrivateRoute path="/admin" component={Admin} />
-        </Switch>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
       </div>
     );
   }
 }
-
-//Home component
-const Home = props => (
-  <div>
-    <h2>Home {console.log(props)}</h2>
-  </div>
-);
-
-//Admin component
-const Admin = ({ match }) => {
-  return (
-    <div>
-      {" "}
-      <h2>Welcome admin </h2>
-    </div>
-  );
-};
-
-//Private router function
-const PrivateRoute = ({ component: Component, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        fakeAuth.isAuthenticated === true ? (
-          <Component {...props} />
-        ) : (
-          <Redirect
-            to={{ pathname: "/login", state: { from: props.location } }}
-          />
-        )}
-    />
-  );
-};
 
 export default App;
