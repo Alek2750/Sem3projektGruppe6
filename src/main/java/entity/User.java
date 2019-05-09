d8fb8366028d905e24089970a16210fce677847f
Alek2750 <alek2750@gmail.com>
2019-05-09 22:09:43 +0200

    User is back to the old data

--- src/main/java/entity/User.java
+++ src/main/java/entity/User.java
@@ -6,8 +6,6 @@ import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
@@ -21,62 +19,24 @@ import org.mindrot.jbcrypt.BCrypt;
 @Table(name = "users")
 public class User implements Serializable {
 
-    private static final long serialVersionUID = 1L;
-    @Id
-    @Basic(optional = false)
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
-    
-    
-    @Column(name = "user_name", length = 25)
-    private String userName;
-    @Basic(optional = false)
-    
-    @Size(min = 1, max = 255)
-    @Column(name = "user_pass")
-    private String userPass;
-    
-    @Size(max = 50)
-    @Column(name = "email", length = 50, unique = true)
-    private String email;
+  private static final long serialVersionUID = 1L;
+  @Id
+  @Basic(optional = false)
+  @NotNull
+  @Column(name = "user_name", length = 25)
+  private String userName;
+  @Basic(optional = false)
+  @NotNull
+  @Size(min = 1, max = 255)
+  @Column(name = "user_pass")
+  private String userPass;
+  @JoinTable(name = "user_roles", joinColumns = {
+    @JoinColumn(name = "user_name", referencedColumnName = "user_name")}, inverseJoinColumns = {
+    @JoinColumn(name = "role_name", referencedColumnName = "role_name")})
+  @ManyToMany
+  private List<Role> roleList = new ArrayList();
 
-    
-    @Size(max = 45)
-    @Column(name = "first_name", length = 15)
-    private String firstName;
-
-    
-    @Size(max = 45)
-    @Column(name = "last_name", length = 15)
-    private String lastName;
-
- 
-    @Size(max = 50)
-    @Column(name = "street", length = 50)
-    private String street;
-
-  
-    @Size(min = 4, max = 4)
-    @Column(name = "zip_code", length = 4)
-    private Integer zipCode;
-
-   
-    @Size(max = 50)
-    @Column(name = "city", length = 50)
-    private String city;
-
-    
-    @Size(min = 8, max = 8)
-    @Column(name = "phone_number", length = 8)
-    private Integer phoneNumber;
-
-    @JoinTable(name = "user_roles", joinColumns = {
-        @JoinColumn(name = "user_name", referencedColumnName = "user_name")}, inverseJoinColumns = {
-        @JoinColumn(name = "role_name", referencedColumnName = "role_name")})
-    @ManyToMany
-    private List<Role> roleList = new ArrayList();
-
-    public List<String> getRolesAsStrings() {
+  public List<String> getRolesAsStrings() {
         if (roleList.isEmpty()) {
             return null;
         }
@@ -129,71 +89,5 @@ public class User implements Serializable {
     public void addRole(Role userRole) {
         roleList.add(userRole);
     }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public Integer getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(Integer zipCode) {
-        this.zipCode = zipCode;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public Integer getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(Integer phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-
 
 }
