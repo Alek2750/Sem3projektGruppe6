74ea3a04d0a7345d51217391f4e517d4c43c51fe
Alek2750 <alek2750@gmail.com>
2019-05-10 13:50:05 +0200

    sql update

---
+++ src/main/resources/META-INF/bookingdata.sql
@@ -0,0 +1,5 @@
+
+INSERT INTO `seed`.`booking` (`startdate`, `enddate`, `car_id`, `total_price`) VALUES ('2019-05-03', '2019-05-10', '1', '5000');
+INSERT INTO `seed`.`booking` (`startdate`, `enddate`, `car_id`, `total_price`) VALUES ('2019-05-18', '2019-05-21', '5', '5000');
+INSERT INTO `seed`.`booking` (`startdate`, `enddate`, `car_id`, `total_price`) VALUES ('2019-06-28', '2019-07-02', '10', '7000');
+INSERT INTO `seed`.`booking` (`startdate`, `enddate`, `car_id`, `total_price`) VALUES ('2019-05-06', '2019-05-07', '7', '7000');
